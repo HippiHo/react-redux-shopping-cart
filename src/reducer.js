ad91bec5d3d4e7c315cb39a757ffad2df6a22e08
Elise Maschke <elise.maschke@outlook.de>
2018-12-10 12:54:32 +0100

    unbroken

--- src/reducer.js
+++ src/reducer.js
@@ -1,31 +1,43 @@
 const initialState = JSON.parse(localStorage.getItem("react-cart")) || {
+  headerdata: {
+    title: "React shopping cart",
+    desc:
+      "Below you find a react shopping cart which persists its data to the LocalStorage. Refactor it using Redux!"
+  },
   data: [
     {
       id: 0,
       name: "JavaScript: The Definitive Guide, 6th Edition",
       release: "September 2010",
-      amount: 1,
+      amount: 0,
       price: "2.99"
     },
     {
       id: 1,
       name: "Ruby on Rails: Up and Running",
       release: "March 2007",
-      amount: 1,
+      amount: 0,
       price: "30.99"
     },
     {
       id: 2,
       name: "MongoDB: The Definitive Guide",
-      release: "Januar 2019",
-      amount: 1,
+      release: "January 2019",
+      amount: 0,
       price: "99.99"
     },
     {
       id: 3,
       name: "Linux Cookbook",
-      release: "Februar 2009",
-      amount: 1,
+      release: "February 2009",
+      amount: 0,
+      price: "24.99"
+    },
+    {
+      id: 4,
+      name: "Eloquent Javascript 3rd Edition",
+      release: "October 2018",
+      amount: 0,
       price: "24.99"
     }
   ],
